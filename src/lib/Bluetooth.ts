99fbb10e50125312e3cecc5d049f149f852774c3
Tobias Emil <tobias@protofi.dk>
2019-05-08 11:49:55 +0200

    bl refactor

--- src/lib/Bluetooth.ts
+++ src/lib/Bluetooth.ts
@@ -1,74 +1,74 @@
 import * as Noble from 'noble'
 import { Advertisement } from 'noble'
 
-export interface ScanFilter {
-	(peripheral: Noble.Peripheral) : boolean
-}
+// export interface ScanFilter {
+// 	(peripheral: Noble.Peripheral) : boolean
+// }
 
-export interface DeviceFoundCallback {
-	(peripheral: Noble.Peripheral) : void
-}
+// export interface DeviceFoundCallback {
+// 	(peripheral: Noble.Peripheral) : void
+// }
 
 export default class Bluetooth {
 	
-	private scanning: boolean = false
+	// private scanning: boolean = false
 
 	private stateChangeActions: Map<string, () => void> = new Map()
 
-	private scannerStartTime = 0
-	private scannerTimeout = 10000 //10 sec
+	// private scannerStartTime = 0
+	// private scannerTimeout = 10000 //10 sec
 
-	private peripheralName: string = "home-cue"
-	private knownPeripherals: Set<string> = new Set()
-	protected currentPeripheral: Noble.Peripheral
+	// private peripheralName: string = "home-cue"
+	// private knownPeripherals: Set<string> = new Set()
+	// protected currentPeripheral: Noble.Peripheral
 
-	private servicesMap = new Map<string, Noble.Service>()
-	private characteristicMap = new Map<string, Map<string, Noble.Characteristic>>()
+	// private servicesMap = new Map<string, Noble.Service>()
+	// private characteristicMap = new Map<string, Map<string, Noble.Characteristic>>()
 
-	public readonly defaultScanFilter: ScanFilter = (peripheral: Noble.Peripheral) => {
-		if(peripheral === undefined) return false
+	// public readonly defaultScanFilter: ScanFilter = (peripheral: Noble.Peripheral) => {
+	// 	if(peripheral === undefined) return false
 
-		const { localName } = peripheral.advertisement
+	// 	const { localName } = peripheral.advertisement
 
-		return localName === this.peripheralName && this.knownPeripherals.has(peripheral.id)
-	}
+	// 	return localName === this.peripheralName && this.knownPeripherals.has(peripheral.id)
+	// }
 
-	public readonly pairingScanFilter: ScanFilter = (peripheral: Noble.Peripheral) => {
-		if(peripheral === undefined) return false
+	// public readonly pairingScanFilter: ScanFilter = (peripheral: Noble.Peripheral) => {
+	// 	if(peripheral === undefined) return false
 
-		const { localName } = peripheral.advertisement
+	// 	const { localName } = peripheral.advertisement
 
-		return localName === this.peripheralName && !this.knownPeripherals.has(peripheral.id)
-	}
+	// 	return localName === this.peripheralName && !this.knownPeripherals.has(peripheral.id)
+	// }
 
-	private currentScanFilter: ScanFilter = this.defaultScanFilter
-	private prevScanFilter: ScanFilter = null
+	// private currentScanFilter: ScanFilter = this.defaultScanFilter
+	// private prevScanFilter: ScanFilter = null
 
-	private scannerStrategy = (peripheral: Noble.Peripheral) => {
-		if(peripheral === undefined) return false
+	// private scannerStrategy = (peripheral: Noble.Peripheral) => {
+	// 	if(peripheral === undefined) return false
 
-		const { localName } = peripheral.advertisement
+	// 	const { localName } = peripheral.advertisement
 
-		if(localName != this.peripheralName) return false
+	// 	if(localName != this.peripheralName) return false
 		
-		if(this.knownPeripherals.has(peripheral.id)) return false
+	// 	if(this.knownPeripherals.has(peripheral.id)) return false
 
-		this.knownPeripherals.add(peripheral.id)
+	// 	this.knownPeripherals.add(peripheral.id)
 
-		if(this.currentDeviceFoundCB)
-			this.currentDeviceFoundCB(peripheral)
+	// 	if(this.currentDeviceFoundCB)
+	// 		this.currentDeviceFoundCB(peripheral)
 
-		return true
-	}
+	// 	return true
+	// }
 	/**
 	 * Callbacks
 	 */
-	private currentDeviceFoundCB: DeviceFoundCallback
-	private prevDeviceFoundCB: DeviceFoundCallback
+	// private currentDeviceFoundCB: DeviceFoundCallback
+	// private prevDeviceFoundCB: DeviceFoundCallback
 	
-	private connectHangupCallback: () => void
-	private audioAlertCallback: (peripheralId: string) => void
-	private peripheralButtonCallback: () => void
+	// private connectHangupCallback: () => void
+	// private audioAlertCallback: (peripheralId: string) => void
+	// private peripheralButtonCallback: () => void
 
 	constructor() {
 		// Noble.on("stateChange", this.onBleStateChange.bind(this))
@@ -80,225 +80,236 @@ export default class Bluetooth {
 
 	private mountHooks()
     {
-        Noble.on("stateChange", (state: string) => {
-            console.log('STATE CHANGE:', state)
-            const action = this.stateChangeActions.get(state)
-            if(action) action()
+		Noble.on("stateChange", this.stateChange.bind(this))
+		Noble.on("discover", 	this.deviceDiscovered.bind(this))
+
+        Noble.on("scanStart", () => {
+            console.log('SCANNING STARTED')
         })
 
-        Noble.on("discover", (peripheral: Noble.Peripheral) => {
-
-        })
-
-        Noble.on("scanStart", (scan: string) => {
-            console.log('SCANNING STARTED', scan)
-        })
-
-        Noble.on("scanStop", (scan: string) => {
-            console.log('SCANNING STOPPED', scan)
+        Noble.on("scanStop", () => {
+            console.log('SCANNING STOPPED')
         })
     }
 
+	private stateChange(state: string)
+	{
+		console.log('STATE CHANGE:', state)
+		const action = this.stateChangeActions.get(state)
+		if(action) action()
+	}
+
+	private deviceDiscovered(peripheral: Noble.Peripheral)
+	{
+
+	}
+
+	/**
+	 * scan
+	 */
+	public scan() {
+		Noble.startScanning([], true) // any service UUID, duplicates allowed
+	}
 
 	/**
 	 * onDev
 	 * 
 	 * iceFound
 	 */
-	public onDeviceFound(cb: DeviceFoundCallback): void {
-		this.currentDeviceFoundCB = cb
-	}
+	// public onDeviceFound(cb: DeviceFoundCallback): void {
+	// 	this.currentDeviceFoundCB = cb
+	// }
 
-	public onConnectHangup(cb: () => void): void {
-		this.connectHangupCallback = cb
-	}
+	// public onConnectHangup(cb: () => void): void {
+	// 	this.connectHangupCallback = cb
+	// }
 
-	public onAudioAlert(cb: (peripheralId: string) => void): void {
-		this.audioAlertCallback = cb
-	}
+	// public onAudioAlert(cb: (peripheralId: string) => void): void {
+	// 	this.audioAlertCallback = cb
+	// }
 
-	public onPeripheralButton(cb: () => void): void {
-		this.peripheralButtonCallback = cb
-	}
+	// public onPeripheralButton(cb: () => void): void {
+	// 	this.peripheralButtonCallback = cb
+	// }
 	
-	public poweredOn(cb: () => void): any {
-        this.stateChangeActions.set("poweredOn", cb)
-    }
+	// public poweredOn(cb: () => void): any {
+    //     this.stateChangeActions.set("poweredOn", cb)
+    // }
 
-	public scan(scanFilter?: ScanFilter, cb?: DeviceFoundCallback, once?: boolean): void {
+	// public scan(scanFilter?: ScanFilter, cb?: DeviceFoundCallback, once?: boolean): void {
 		
-		console.log('STARTING SCAN')
+	// 	console.log('STARTING SCAN')
 
-		// if(Noble.state !== "poweredOn")
-		// {
-		// 	console.error("DENIED: Radio is not powered on")
-		// 	this.scanning = false
-		// 	return
-		// }
+	// 	// if(Noble.state !== "poweredOn")
+	// 	// {
+	// 	// 	console.error("DENIED: Radio is not powered on")
+	// 	// 	this.scanning = false
+	// 	// 	return
+	// 	// }
 
-		// if(this.scanning)
-		// {
-		// 	console.error("DENIED: Already scanning")
-		// 	return
-		// }
+	// 	// if(this.scanning)
+	// 	// {
+	// 	// 	console.error("DENIED: Already scanning")
+	// 	// 	return
+	// 	// }
 		
-		// this.prevScanFilter 		= (once) ? this.currentScanFilter : null
-		// this.currentScanFilter 		= (scanFilter) ? scanFilter : this.defaultScanFilter
+	// 	// this.prevScanFilter 		= (once) ? this.currentScanFilter : null
+	// 	// this.currentScanFilter 		= (scanFilter) ? scanFilter : this.defaultScanFilter
 
-		// if(cb) //if callback if parsed
-		// {
-		// 	console.log('SETTING SCAN CALLBACK')
-		// 	this.prevDeviceFoundCB = (once) ? this.currentDeviceFoundCB : null
-		// 	this.currentDeviceFoundCB = cb
-		// }
+	// 	// if(cb) //if callback if parsed
+	// 	// {
+	// 	// 	console.log('SETTING SCAN CALLBACK')
+	// 	// 	this.prevDeviceFoundCB = (once) ? this.currentDeviceFoundCB : null
+	// 	// 	this.currentDeviceFoundCB = cb
+	// 	// }
 	
-		// this.scannerStartTime = Date.now()
-		Noble.startScanning([], true) // any service UUID, duplicates allowed
-		// this.scanning = true
-	}
+	// 	// this.scannerStartTime = Date.now()
+	// 	Noble.startScanning([], true) // any service UUID, duplicates allowed
+	// 	// this.scanning = true
+	// }
 
-	private deviceFound(peripheral: Noble.Peripheral) 
-	{
-		if(!this.scannerStrategy(peripheral) && this.scannerStartTime + this.scannerTimeout > Date.now()) return
+	// private deviceFound(peripheral: Noble.Peripheral) 
+	// {
+	// 	if(!this.scannerStrategy(peripheral) && this.scannerStartTime + this.scannerTimeout > Date.now()) return
 
-		// if (!this.currentScanFilter(peripheral)) return
-		// if (peripheral.state !== "disconnected") return
+	// 	if (!this.currentScanFilter(peripheral)) return
+	// 	if (peripheral.state !== "disconnected") return
 
-		this.stopScaning()
+	// 	this.stopScaning()
 
-		// /**
-		//  * Advertisement holds data that we can access 
-		//  * without having to discover services
-		//  */
-		// this.logAdvertisementData(peripheral);
-		// /**
-		//  * Investigate service data, to see if audio or button trigger is present. 
-		//  */
-		// const serviceDataJSONArray = JSON.parse(JSON.stringify(peripheral.advertisement.serviceData))
+	// 	/**
+	// 	 * Advertisement holds data that we can access 
+	// 	 * without having to discover services
+	// 	 */
+	// 	this.logAdvertisementData(peripheral);
+	// 	/**
+	// 	 * Investigate service data, to see if audio or button trigger is present. 
+	// 	 */
+	// 	const serviceDataJSONArray = JSON.parse(JSON.stringify(peripheral.advertisement.serviceData))
 	
-		// if(serviceDataJSONArray.length < 1) {
-		// 	return
-		// }
+	// 	if(serviceDataJSONArray.length < 1) {
+	// 		return
+	// 	}
 		
-		// console.log("Service data: ", serviceDataJSONArray);
+	// 	console.log("Service data: ", serviceDataJSONArray);
 
-		// const trigger = serviceDataJSONArray[0].uuid
+	// 	const trigger = serviceDataJSONArray[0].uuid
 
-		// if(trigger === "4f49445541") {
-		// 	this.audioAlertCallback(peripheral.id)
-		// }
+	// 	if(trigger === "4f49445541") {
+	// 		this.audioAlertCallback(peripheral.id)
+	// 	}
 	
-		// if(trigger === "4e4f54545542") {
-		// 	this.peripheralButtonCallback()
-		// }
-		// this.connectPeripheral(peripheral)
-	}
+	// 	if(trigger === "4e4f54545542") {
+	// 		this.peripheralButtonCallback()
+	// 	}
+	// 	this.connectPeripheral(peripheral)
+	// }
 
-	public stopScaning() {
-		console.log('STOP SCANNING')
-		Noble.stopScanning()
-		this.scanning = false
-	}
+	// public stopScaning() {
+	// 	console.log('STOP SCANNING')
+	// 	Noble.stopScanning()
+	// 	this.scanning = false
+	// }
 
-	public setScanFilter(scanFilter: ScanFilter) {
-		this.currentScanFilter = scanFilter
-	}
+	// public setScanFilter(scanFilter: ScanFilter) {
+	// 	this.currentScanFilter = scanFilter
+	// }
 
-	private setScanStarted() {
-		this.scanning = true
-	}
+	// private setScanStarted() {
+	// 	this.scanning = true
+	// }
 
-	private setScanStopped() {
-		this.scanning = false
-	}
+	// private setScanStopped() {
+	// 	this.scanning = false
+	// }
 
-	private onBleStateChange(state: string) {
+	// private onBleStateChange(state: string) {
 	
-		console.log('STATE CHANGE: ', state)
-		const action = this.stateChangeActions.get(state)
-		if(action) action()
+	// 	console.log('STATE CHANGE: ', state)
+	// 	const action = this.stateChangeActions.get(state)
+	// 	if(action) action()
 
-	}
+	// }
 
-	private logAdvertisementData(discPeripheral: Noble.Peripheral) {
-		const txPowerLevel: number = discPeripheral.rssi
-		const localName: string = discPeripheral.advertisement.localName
+	// private logAdvertisementData(discPeripheral: Noble.Peripheral) {
+	// 	const txPowerLevel: number = discPeripheral.rssi
+	// 	const localName: string = discPeripheral.advertisement.localName
 		
-		if (localName) {
-			console.log("local name: " + localName)	
-		}
-		if(txPowerLevel) {
-			console.log("tx level: ", txPowerLevel)
-		}
+	// 	if (localName) {
+	// 		console.log("local name: " + localName)	
+	// 	}
+	// 	if(txPowerLevel) {
+	// 		console.log("tx level: ", txPowerLevel)
+	// 	}
 		
-	}
+	// }
 
-	private connectPeripheral(discPeripheral: Noble.Peripheral) {
-		this.currentPeripheral = discPeripheral
-		this.currentPeripheral.once("connect", this.onPeripheralConnect.bind(this))
-		this.currentPeripheral.once("disconnect", this.onPeripheralDisconnect.bind(this))
+	// private connectPeripheral(discPeripheral: Noble.Peripheral) {
+	// 	this.currentPeripheral = discPeripheral
+	// 	this.currentPeripheral.once("connect", this.onPeripheralConnect.bind(this))
+	// 	this.currentPeripheral.once("disconnect", this.onPeripheralDisconnect.bind(this))
 
-		// handle once request
-		this.currentScanFilter = (this.prevScanFilter) ? this.prevScanFilter : this.currentScanFilter
-		this.prevScanFilter = null
+	// 	// handle once request
+	// 	this.currentScanFilter = (this.prevScanFilter) ? this.prevScanFilter : this.currentScanFilter
+	// 	this.prevScanFilter = null
 
-		this.currentPeripheral.connect(error => console.log)
-		this.watchForConnectionTimeout()
-	}
+	// 	this.currentPeripheral.connect(error => console.log)
+	// 	this.watchForConnectionTimeout()
+	// }
 
-	private onPeripheralConnect(keepConnectionAlive: boolean) {
-		if(!this.currentPeripheral) {
-			return
-		}
-		console.log(`* Connected to: ${this.currentPeripheral.advertisement.localName}`)
-		if(this.currentDeviceFoundCB) this.currentDeviceFoundCB(this.currentPeripheral)
+	// private onPeripheralConnect(keepConnectionAlive: boolean) {
+	// 	if(!this.currentPeripheral) {
+	// 		return
+	// 	}
+	// 	console.log(`* Connected to: ${this.currentPeripheral.advertisement.localName}`)
+	// 	if(this.currentDeviceFoundCB) this.currentDeviceFoundCB(this.currentPeripheral)
 
-		this.currentPeripheral.discoverAllServicesAndCharacteristics((error, services, chararacteristics) => {
-			if(error) {
-				console.log("There was an error discovering services: ", error)
-			}
-			else {
-				this.disconnectPeripheral()
-			}
-			// if(services) populateServiceMap(services)
-		})
-	}
+	// 	this.currentPeripheral.discoverAllServicesAndCharacteristics((error, services, chararacteristics) => {
+	// 		if(error) {
+	// 			console.log("There was an error discovering services: ", error)
+	// 		}
+	// 		else {
+	// 			this.disconnectPeripheral()
+	// 		}
+	// 		// if(services) populateServiceMap(services)
+	// 	})
+	// }
 
-	public disconnectPeripheral() {
-		if(!this.currentPeripheral) {
-			console.log("Disconnect: No peripheral to disconnect.")
-			return
-		}
-		this.currentPeripheral.disconnect()
-	}
+	// public disconnectPeripheral() {
+	// 	if(!this.currentPeripheral) {
+	// 		console.log("Disconnect: No peripheral to disconnect.")
+	// 		return
+	// 	}
+	// 	this.currentPeripheral.disconnect()
+	// }
 
-	private onPeripheralDisconnect() {
-		this.characteristicMap = new Map<string, Map<string, Noble.Characteristic>>()
-		this.servicesMap = new Map<string, Noble.Service>()
+	// private onPeripheralDisconnect() {
+	// 	this.characteristicMap = new Map<string, Map<string, Noble.Characteristic>>()
+	// 	this.servicesMap = new Map<string, Noble.Service>()
 	
-		if(this.currentPeripheral) {
-			console.log(`Disconnecting from: ${this.currentPeripheral.advertisement.localName}`)
-		}
-		delete this.currentPeripheral
+	// 	if(this.currentPeripheral) {
+	// 		console.log(`Disconnecting from: ${this.currentPeripheral.advertisement.localName}`)
+	// 	}
+	// 	delete this.currentPeripheral
 		
-		this.scan(this.currentScanFilter, this.currentDeviceFoundCB)
-	}
+	// 	this.scan(this.currentScanFilter, this.currentDeviceFoundCB)
+	// }
 
-	private watchForConnectionTimeout() {
-		let currentTimeoutDuration = 0
-		const ConnectionTimeoutInterval = setInterval(() => {
+	// private watchForConnectionTimeout() {
+	// 	let currentTimeoutDuration = 0
+	// 	const ConnectionTimeoutInterval = setInterval(() => {
 		
-		if(this.currentPeripheral && (this.currentPeripheral.state === "connecting" || this.currentPeripheral.state === "disconnecting"))
-		{
-			currentTimeoutDuration++
-			console.log(this.currentPeripheral.state)
-		}
+	// 	if(this.currentPeripheral && (this.currentPeripheral.state === "connecting" || this.currentPeripheral.state === "disconnecting"))
+	// 	{
+	// 		currentTimeoutDuration++
+	// 		console.log(this.currentPeripheral.state)
+	// 	}
 
-		if(currentTimeoutDuration > 10) {
-			this.connectHangupCallback()
-			currentTimeoutDuration = 0
-			clearInterval(ConnectionTimeoutInterval)
-		}
-		}, 1000)
-	}
+	// 	if(currentTimeoutDuration > 10) {
+	// 		this.connectHangupCallback()
+	// 		currentTimeoutDuration = 0
+	// 		clearInterval(ConnectionTimeoutInterval)
+	// 	}
+	// 	}, 1000)
+	// }
 }
